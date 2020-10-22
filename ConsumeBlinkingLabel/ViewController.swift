257a7e8b891ccee2e477ed4f593e04bb2936f7fb
Casey McGuire <casey@steepturnsoftware.com>
2020-10-22 15:34:22 -0500

    first commit

---
+++ ConsumeBlinkingLabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsumeBlinkingLabel
+//
+//  Created by Casey McGuire on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
